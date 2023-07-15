da719275ef2913929f4c91cdcb8a4f9bca4efcd8
Jay Sm <jaysmailg14@gmail.com>
2023-07-15 13:58:27 -0400

    fully functional + moved quotes to backend file

---
+++ javascripts/app.js
@@ -0,0 +1,22 @@
+let quote;
+
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function getNewQuote() {
+  const response = await fetch("/backend/quotes.json");
+  const quotes = await response.json();
+  quote = quotes[getRandom(50)];
+
+  return quote;
+}
+
+const button = document.querySelector("button");
+const quoteDisplay = document.querySelector("p");
+
+button.addEventListener("click", () => {
+  getNewQuote().then(() => {
+    quoteDisplay.innerHTML = quote;
+  });
+});
